ac30875f69a21c1c747cdaee85cf92a022e884a5
jacqueline spineli <pp.spineli@gmail.com>
2026-03-24 19:45:32 -0300

    alteração nas headlines e titulo do quiz

--- app/odml/[version]/v1/headline.tsx
+++ app/odml/[version]/v1/headline.tsx
@@ -8,6 +8,26 @@ interface IHeadline {
   text: ReactNode;
 }
 
+const subHeadline = (
+  <div>
+    <p className="md:hidden">
+      E o caminho para se tornar uma MULHER LIVRE: prosperar, construir uma
+      vida estável e ter um relacionamento saudável, sem carregar o mundo
+      nas costas.
+    </p>
+
+    <div className="hidden md:block">
+      <p>E o caminho para se tornar uma MULHER LIVRE:</p>
+      <p>prosperar, construir uma vida estável e ter um relacionamento</p>
+      <p>saudável, sem carregar o mundo nas costas.</p>
+    </div>
+
+    <p className="mt-2 text-[#C0964B] font-bold">
+      Faça sua inscrição gratuita
+    </p>
+  </div>
+);
+
 export const Headline: IHeadline[] = [
   {
     id: "h0",
@@ -24,24 +44,38 @@ export const Headline: IHeadline[] = [
         financeira...
       </p>
     ),
-    text: (
-      <div>
-        <p className="md:hidden">
-          E o caminho para se tornar uma MULHER LIVRE: prosperar, construir uma
-          vida estável e ter um relacionamento saudável, sem carregar o mundo
-          nas costas.
-        </p>
+    text: subHeadline,
+  },
 
-        <div className="hidden md:block">
-          <p>E o caminho para se tornar uma MULHER LIVRE:</p>
-          <p>prosperar, construir uma vida estável e ter um relacionamento</p>
-          <p>saudável, sem carregar o mundo nas costas.</p>
-        </div>
-
-        <p className="mt-2 text-[#C0964B] font-bold">
-          Faça sua inscrição gratuita
-        </p>
-      </div>
+  {
+    id: "h1",
+    isPicture: false,
+    isLogo: true,
+    title: (
+      <p className="uppercase font-spectral text-[#D3CAC0] font-extrabold">
+        Aprenda a identificar os relacionamentos
+        <br className="hidden md:block" />
+        que estão limitando sua conta bancária.
+      </p>
     ),
+    text: subHeadline,
+  },
+
+  {
+    id: "h2",
+    isPicture: false,
+    isLogo: true,
+    title: (
+      <p className="uppercase font-spectral text-[#D3CAC0] font-extrabold">
+        Identifique e rompa os
+        <br className="hidden md:block" />
+        padrões que estão travando
+        <br className="hidden md:block" />
+        os seus relacionamentos
+        <br className="hidden md:block" />
+        e a sua vida financeira.
+      </p>
+    ),
+    text: subHeadline,
   },
 ];
